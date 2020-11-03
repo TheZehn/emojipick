b8ced4cc0ce18bfd8c9cc282deaf0a1be838670f
Zachary Ehnerd <zachary@ehnerd.com>
2020-11-03 16:18:42 -0500

    Emojis grouped by category!

--- src/App.js
+++ src/App.js
@@ -4,36 +4,64 @@ import data from './emoji.json';
 
 function App() {
 	const [searchTerm, setSearchTerm] = React.useState("");
+	const [results, setResults] = React.useState([]);
 
 	const handleChange = event => {
-		setSearchTerm(event.target.value.toLowerCase());
+		let searchTerm = event.target.value.toLowerCase();
+		setSearchTerm(searchTerm);
+		let filtered = data.filter( (em) => {
+				let emName = em.name.toLowerCase();
+				let emCat = em.category.toLowerCase();
+
+				return (emCat.includes(searchTerm) || emName.includes(searchTerm)) && searchTerm !== "";
+			});
+		setResults(groupBy(filtered,"group"));
 	};
+
+	const groupBy = (array, key) => {  
+	  return array.reduce((result, currentValue) => {
+	        (result[currentValue[key]] = result[currentValue[key]] || []).push(currentValue);
+	        return result;
+	  }, {});
+	};
+
 
   return (
     <div className="App">
 		<Box display="flex">	
 			<Box m="auto"	>
 	      <header className="App-header">
-					<TextField id="peruse" placeholder="Search" value={searchTerm} onChange={handleChange} />
+					<TextField id="peruse" placeholder="Emoji Search" value={searchTerm} onChange={handleChange} />
 	      </header>
-			</Box></Box>
-			<Box display="flex" m={3}>
-			<Box m="auto">
-				{ data.filter((p) => {
-						let emojiName = p.name.toLowerCase();
-						let emojiCat = p.category.toLowerCase();
-						return (emojiCat.includes(searchTerm) || emojiName.includes(searchTerm)) && searchTerm !== "";
-					})
-					.map((p) => {
-						return (
-							<Emoji key={p.codes} em={p} />
-							);
-						})
-				}
 			</Box>
 		</Box>
+			<Box display="flex" m={3}>
+				<Box m="auto">
+				{  Object.entries(results).map( cat => {
+						return <EmojiCat key={cat[0]} data={cat}/>
+				})}
+				
+				</Box>
+			</Box>
     </div>
   );
+
+}
+
+
+function EmojiCat(props){
+	let catName = props.data[0];
+	let emojis = props.data[1];
+
+	return (
+		<React.Fragment>
+			<Typography variant="h5">{catName}</Typography>
+		 { emojis.map( emoji =>  {
+				return <Emoji key={emoji.codes} em={emoji} />
+			})}	
+		</React.Fragment>
+		)
+
 }
 
 
@@ -43,7 +71,7 @@ function Emoji(props){
 
 	return (
 	 <Tooltip title={em.name} >
-			<Typography style={{display: 'inline-block'}} variant='h4' >{em.char} </Typography>
+			<Typography style={{display: 'inline-block'}} variant='h4'> {em.char} </Typography>
 	</Tooltip>
 	)
 }
